scale AmpGen amplitudes by their averages wherever they're used; also fix the mixing toy to actually work

k3pi_efficiency/lib_efficiency/mixing.py
@@ -22,7 +22,7 @@ MixingParams = namedtuple(
 )
 
 
-def _propagate(times: np.ndarray, d_mass: float, d_width: float) -> np.ndarray:
+def _propagate(*, times: np.ndarray, d_mass: float, d_width: float) -> np.ndarray:
     """
     Time propagation part of the mixing functions
 
@@ -31,16 +31,21 @@ def _propagate(times: np.ndarray, d_mass: float, d_width: float) -> np.ndarray:
 
 
 def _phase_arg(
-    times: np.ndarray, d_width: float, mixing_x: complex, mixing_y: complex
+    *,
+    times: np.ndarray,
+    d_width: float,
+    d_mass: float,
+    mixing_x: complex,
+    mixing_y: complex,
 ) -> np.ndarray:
     """
     Part that goes inside the sin/cos
 
     """
-    return d_width * times * (mixing_x - mixing_y * 1j) / 2
+    return times * (mixing_x * d_mass - mixing_y * 1j * d_width) / 2
 
 
-def _g_plus(times: np.ndarray, params: MixingParams) -> np.ndarray:
+def _g_plus(*, times: np.ndarray, params: MixingParams) -> np.ndarray:
     """
     Time-dependent mixing function
 
@@ -53,12 +58,20 @@ def _g_plus(times: np.ndarray, params: MixingParams) -> np.ndarray:
     :returns: array of complex
 
     """
-    return _propagate(times, params.d_mass, params.d_width) * np.cos(
-        _phase_arg(times, params.d_width, params.mixing_x, params.mixing_y)
+    return _propagate(
+        times=times, d_mass=params.d_mass, d_width=params.d_width
+    ) * np.cos(
+        _phase_arg(
+            times=times,
+            d_width=params.d_width,
+            d_mass=params.d_mass,
+            mixing_x=params.mixing_x,
+            mixing_y=params.mixing_y,
+        )
     )
 
 
-def _g_minus(times: np.ndarray, params: MixingParams) -> np.ndarray:
+def _g_minus(*, times: np.ndarray, params: MixingParams) -> np.ndarray:
     """
     Time-dependent mixing function
 
@@ -72,13 +85,21 @@ def _g_minus(times: np.ndarray, params: MixingParams) -> np.ndarray:
 
     """
     return (
-        _propagate(times, params.d_mass, params.d_width)
+        _propagate(times=times, d_mass=params.d_mass, d_width=params.d_width)
         * 1j
-        * np.sin(_phase_arg(times, params.d_width, params.mixing_x, params.mixing_y))
+        * np.sin(
+            _phase_arg(
+                times=times,
+                d_width=params.d_width,
+                d_mass=params.d_mass,
+                mixing_x=params.mixing_x,
+                mixing_y=params.mixing_y,
+            )
+        )
     )
 
 
-def _good_p_q(p: complex, q: complex) -> bool:
+def _good_p_q(*, p: complex, q: complex) -> bool:
     """
     Check mag of p+q is 1
 
@@ -88,6 +109,7 @@ def _good_p_q(p: complex, q: complex) -> bool:
 
 
 def mixed_d0_coeffs(
+    *,
     times: np.ndarray,
     p: complex,
     q: complex,
@@ -108,14 +130,15 @@ def mixed_d0_coeffs(
     :returns: Dbar0 coefficient at the times provided
 
     """
-    assert _good_p_q(p, q)
+    assert _good_p_q(p=p, q=q)
     return (
-        _g_plus(times, params),
-        q * _g_minus(times, params) / p,
+        _g_plus(times=times, params=params),
+        q * _g_minus(times=times, params=params) / p,
     )
 
 
 def mixed_dbar0_coeffs(
+    *,
     times: np.ndarray,
     p: complex,
     q: complex,
@@ -136,8 +159,10 @@ def mixed_dbar0_coeffs(
     :returns: Dbar0 coefficient at the times provided
 
     """
-    assert _good_p_q(p, q)
-    return p * _g_minus(times, params) / q, _g_plus(times, params)
+    assert _good_p_q(p=p, q=q)
+    return p * _g_minus(times=times, params=params) / q, _g_plus(
+        times=times, params=params
+    )
 
 
 def _lifetimes2invmev(lifetimes: np.ndarray) -> np.ndarray:
@@ -172,8 +197,8 @@ def _ws_weights(
     # Convert lifetimes to inverse MeV
     t_invmev = _lifetimes2invmev(times)
 
-    g_plus = _g_plus(t_invmev, params)
-    g_minus = _g_minus(t_invmev, params)
+    g_plus = _g_plus(times=t_invmev, params=params)
+    g_minus = _g_minus(times=t_invmev, params=params)
 
     q, p = q_p
     num = g_plus * d0_amplitudes + q * g_minus * dbar0_amplitudes / p
@@ -210,7 +235,7 @@ def ws_mixing_weights(
     assert k_charge == 1
 
     # Evaluate amplitudes
-    cf_amplitudes = amplitudes.cf_amplitudes(*k3pi, k_charge) / amplitudes.CF_AVG
-    dcs_amplitudes = amplitudes.dcs_amplitudes(*k3pi, k_charge) / amplitudes.DCS_AVG
+    cf_amplitudes = amplitudes.cf_amplitudes(*k3pi, k_charge)
+    dcs_amplitudes = amplitudes.dcs_amplitudes(*k3pi, k_charge)
 
     return _ws_weights(t_lifetimes, dcs_amplitudes, cf_amplitudes, mixing_params, q_p)

k3pi_efficiency/scripts/mixing/ampgen_test.py
@@ -8,19 +8,19 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 from fourbody.param import helicity_param
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[0]))
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
-sys.path.append(str(pathlib.Path(__file__).resolve().parents[3] / "k3pi-fitter"))
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[3] / "k3pi_fitter"))
 import pdg_params
 from lib_efficiency import efficiency_util, mixing
 from lib_efficiency.plotting import phsp_labels
-from lib_efficiency.phsp_binning import coherence_factor
 from lib_efficiency.amplitude_models import amplitudes
 from lib_time_fit import util as fit_util
-from lib_time_fit import fitter, plotting
+from lib_time_fit import fitter, plotting, models
 
 
 def _weight_hist(weights: np.ndarray) -> None:
@@ -29,47 +29,50 @@ def _weight_hist(weights: np.ndarray) -> None:
     plt.show()
 
 
-def _z(r_d: float, dataframe: pd.DataFrame, weights: np.ndarray):
+def _z(dataframe: pd.DataFrame, weights: np.ndarray) -> complex:
     """
     Coherence factor given the desired amplitude ratio
 
     """
     k, pi1, pi2, pi3 = efficiency_util.k_3pi(dataframe)
 
+    # I don't think it actually matters if we scale the amplitudes here
     cf = amplitudes.cf_amplitudes(k, pi1, pi2, pi3, +1) / amplitudes.CF_AVG
-    # TODO try rotating
-    # TODO try scaling a bit
-    dcs = amplitudes.dcs_amplitudes(k, pi1, pi2, pi3, +1) * r_d / amplitudes.DCS_AVG
+    dcs = amplitudes.dcs_amplitudes(k, pi1, pi2, pi3, +1) / amplitudes.DCS_AVG
 
     # Find Z and integrals
     z = np.sum(cf * dcs.conjugate() * weights)
     num_dcs = np.sum((np.abs(dcs) ** 2) * weights)
     num_cf = np.sum((np.abs(cf) ** 2) * weights)
 
-    # Find R, d and return
-    R = abs(z) / np.sqrt(num_dcs * num_cf)
-    d = np.angle(z, deg=True)
-
-    return R, d
+    return z / np.sqrt(num_dcs * num_cf)
 
 
-def _bc_params(
-    dataframe: pd.DataFrame,
-    weights: np.ndarray,
-    params: mixing.MixingParams,
-    r_d: float,
-) -> Tuple[float, float]:
+def _rd(*, x: float, y: float, z: complex, n_dcs: float, n_cf: float) -> float:
     """
-    Time ratio params, assuming rD = 1 and times in lifetimes
+    Expected rD from the other parameters in the equation and
+    the DCS and CF statistics
+
+    Finds rD by equating int(DCS rate) / nDCS = int(CF rate) / nCF
+
+    Performs the integrals to infinity
 
     """
-    z_mag, z_phase = _z(r_d, dataframe, weights)
-    z_re = z_mag * np.cos(np.pi + z_phase)
-    z_im = z_mag * np.sin(np.pi + z_phase)
+    # CF rate is just exponential
+    cf_integral = 1
 
-    return (params.mixing_x * z_im + params.mixing_y * z_re), (
-        params.mixing_x**2 + params.mixing_y**2
-    ) / 4
+    def dcs_integral(r_d: float):
+        abc_params = models.abc_scan(
+            fit_util.ScanParams(r_d=r_d, x=x, y=y, re_z=z.real, im_z=z.imag)
+        )
+        # Assume the integral evaluated at infinity is 0... otherwise we run into trouble...?
+        return -models._ws_integral_dispatcher(0, *abc_params)
+
+    def target_fcn(r_d: float):
+        return dcs_integral(r_d) - cf_integral * n_dcs / n_cf
+
+    # Initial guess is just the sqrt of ratio of counts
+    return fsolve(target_fcn, x0=np.sqrt(n_dcs / n_cf))[0]
 
 
 def _ratio_err(
@@ -97,8 +100,7 @@ def _time_plot(
     Plot ratio of WS/RS decay times
 
     """
-    bins = np.linspace(0, 8, 15)
-    bins = np.append(bins, 17.71)
+    bins = np.concatenate((np.linspace(0, 7, 10), [11, 18]))
     centres = (bins[1:] + bins[:-1]) / 2
     widths = (bins[1:] - bins[:-1]) / 2
 
@@ -117,23 +119,40 @@ def _time_plot(
         fmt="r+",
     )
 
-    initial_guess = fit_util.MixingParams(1, 1, 1)
-    weighted_minuit = fitter.no_constraints(
-        weighted_ratio, weighted_err, bins, initial_guess
-    )
-
     # No Mixing
     best_val, _ = fitter.weighted_mean(ratio, err)
     plotting.no_mixing(ax, best_val, "k--")
 
     # Mixing
+    initial_guess = fit_util.MixingParams(1, 1, 1)
+    weighted_minuit = fitter.no_constraints(
+        weighted_ratio, weighted_err, bins, initial_guess
+    )
     print(weighted_minuit)
     plotting.no_constraints(ax, weighted_minuit.values, "r--", "Fit (mixing)")
 
     # Actual value
-    # rD of 1
-    ideal = (best_val, *_bc_params(dcs_df, dcs_wt, params, 1))
-    plotting.no_constraints(ax, ideal, "k--", "True")
+    # We can find the expected x and y easily - they're the mixing parameters
+    expected_x = params.mixing_x
+    expected_y = params.mixing_y
+
+    # Find expected Z with a numerical integral
+    expected_z = _z(dcs_df, dcs_wt)
+
+    # Find expected rD by integrating the rates
+    expected_rd = _rd(
+        x=expected_x, y=expected_y, z=expected_z, n_dcs=np.sum(dcs_wt), n_cf=len(cf_df)
+    )
+
+    ideal = fit_util.ScanParams(
+        r_d=expected_rd,
+        x=expected_x,
+        y=expected_y,
+        re_z=expected_z.real,
+        im_z=expected_z.imag,
+    )
+    print(f"{ideal=}")
+    plotting.scan_fit(ax, ideal, "k--", "True")
 
     ax.set_xlabel(r"$\frac{t}{\tau}$")
     ax.set_ylabel(r"$\frac{WS}{RS}$")

test/test_fitter.py
@@ -18,8 +18,7 @@ def test_rs_integral():
 
 def test_ws_integral():
     """
-    Check we get the right integral by comparing with scipy
-    numerical integral
+    Check we get the right integral by comparing with analytic integral
 
     """
     lims = np.array([0.0, 1.0])