ampgen test no longer uses analytical effieicny - fix a small bug in the scaling code but it looks like its still a bit broken

k3pi-data/scripts/add_ampgen_efficiency.py
@@ -65,8 +65,8 @@ def _add_col(rng: np.random.Generator, sign: str, axes: plt.Axes):
         print(f"overwriting {sign} column")
 
     # Use rejection sampling to see which events are kept
-    time_factor = 0.98 if sign == "dcs" else 1.0
-    phsp_factor = 1.0 if sign == "dcs" else 0.9  # unused internally
+    time_factor = 0.9 if sign == "dcs" else 1.0
+    phsp_factor = 1.0 if sign == "dcs" else 0.9
     kept = mock_efficiency.accepted(
         rng, dataframe, time_factor=time_factor, phsp_factor=phsp_factor
     )

k3pi_efficiency/lib_efficiency/efficiency_util.py
@@ -199,7 +199,7 @@ def scale_weights(
 
     # Find the average of each type of weight
     rs_avg = np.sum(rs_wts) / n_rs
-    ws_avg = np.sum(ws_wts) / n_rs
+    ws_avg = np.sum(ws_wts) / n_ws
 
     # We want sum of WS wts to equal N WS generated evts,
     # which is equivalent to N WS reco evts / avg eff

k3pi_efficiency/lib_efficiency/mock_efficiency.py
@@ -33,6 +33,7 @@ def time_efficiency(dataframe: pd.DataFrame, factor: float = 1.0) -> np.ndarray:
     )
 
     assert np.max(retvals) < 1.0
+    assert np.min(retvals) >= 0.0
 
     return retvals
 
@@ -49,9 +50,6 @@ def phsp_efficiency(dataframe: pd.DataFrame, factor: float = 1.0) -> np.ndarray:
     :raises AssertionError: if the efficiencies exceed 1 somehow
 
     """
-    # No phase space efficiency
-    return np.ones(len(dataframe))
-
     # Find the transverse momentum of the kaon
     k_pt = np.sqrt(dataframe["Kplus_Px"] ** 2 + dataframe["Kplus_Py"] ** 2)
 