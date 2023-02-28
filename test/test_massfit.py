d78abf1b0ed183c1dfe389a5fff5bedc66089d5d
Richard Lane <richard.lane@bristol.ac.uk>
2023-02-28 17:23:05 +0000

    fix the tests that have been broken for ages

--- test/test_massfit.py
+++ test/test_massfit.py
@@ -9,7 +9,7 @@ from scipy.integrate import quad
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1] / "k3pi_mass_fit"))
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1] / "k3pi-data"))
-from libFit import pdfs, definitions
+from libFit import pdfs, definitions, util
 from lib_data import stats
 
 
@@ -18,10 +18,29 @@ def test_bkg_normalised():
     Check the background pdf is normalised
 
     """
+    domain = pdfs.domain()
     assert np.isclose(
-        quad(pdfs.normalised_bkg, *pdfs.domain(), args=pdfs.background_defaults("dcs"))[
-            0
-        ],
+        quad(
+            pdfs.normalised_bkg,
+            *domain,
+            args=(*util.sqrt_bkg_param_guess("dcs"), domain)
+        )[0],
+        1.0,
+    )
+
+
+def test_bkg_normalised_reduced():
+    """
+    Check the background pdf is normalised over the reduced domain
+
+    """
+    domain = pdfs.reduced_domain()
+    assert np.isclose(
+        quad(
+            pdfs.normalised_bkg,
+            *domain,
+            args=(*util.sqrt_bkg_param_guess("dcs"), domain)
+        )[0],
         1.0,
     )
 
@@ -32,9 +51,24 @@ def test_signal_normalised():
 
     """
     params = (146, 0.2, 0.2, 0.2, 0.2, 0.002)
-    points = definitions.mass_bins(1000)
+    domain = pdfs.domain()
+    points = np.linspace(*domain, 1000)
 
-    integral = stats.integral(points, pdfs.normalised_signal(points, *params))
+    integral = stats.integral(points, pdfs.normalised_signal(points, *params, domain))
+
+    assert np.isclose(integral, 1.0)
+
+
+def test_signal_normalised_reduced_domain():
+    """
+    Check the signal pdf is normalised
+
+    """
+    params = (146, 0.2, 0.2, 0.2, 0.2, 0.002)
+    domain = pdfs.reduced_domain()
+    points = np.linspace(*domain, 1000)
+
+    integral = stats.integral(points, pdfs.normalised_signal(points, *params, domain))
 
     assert np.isclose(integral, 1.0)
 
@@ -48,9 +82,27 @@ def test_model_normalised():
     n_bkg = 10000
 
     params = (146, 0.2, 0.2, 0.2, 0.2, 0.002, 0.004, -0.001)
-    points = definitions.mass_bins(1000)
+    domain = pdfs.domain()
+    points = np.linspace(*domain, 1000)
 
-    integral = stats.integral(points, pdfs.model(points, n_sig, n_bkg, *params))
+    integral = stats.integral(points, pdfs.model(points, n_sig, n_bkg, *params, domain))
+
+    assert np.isclose(integral, n_sig + n_bkg)
+
+
+def test_model_normalised():
+    """
+    Check that the pdf is normalised correctly over the reduced domain
+
+    """
+    n_sig = 1000
+    n_bkg = 10000
+
+    params = (146, 0.2, 0.2, 0.2, 0.2, 0.002, 0.004, -0.001)
+    domain = pdfs.reduced_domain()
+    points = np.linspace(*domain, 1000)
+
+    integral = stats.integral(points, pdfs.model(points, n_sig, n_bkg, *params, domain))
 
     assert np.isclose(integral, n_sig + n_bkg)
 
@@ -60,68 +112,36 @@ def test_alt_bkg_normalised():
     Check that the alternate background PDF is normalised
 
     """
-    a, b = pdfs.domain()
-    domain_width = b - a
+    domain = pdfs.domain()
+    domain_width = domain[1] - domain[0]
 
     # Pretend the estimated bkg is a constant
     estimated_bkg_pdf = lambda x: 1 / domain_width
 
     assert np.isclose(
-        quad(lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, 0, 0, 0), a, b)[0], 1.0
-    )
-
-    assert np.isclose(
-        quad(lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, 3, 0, 0), a, b)[0], 1.0
-    )
-
-    assert np.isclose(
-        quad(lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, 3, 2, 0), a, b)[0], 1.0
-    )
-
-    assert np.isclose(
-        quad(lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, 3, 2, 1), a, b)[0], 1.0
-    )
-
-
-def test_reduced_bkg_normalised():
-    """
-    Check the reduced background pdf is normalised
-
-    """
-    assert np.isclose(
         quad(
-            pdfs.normalised_bkg_reduced,
-            *pdfs.reduced_domain(),
-            args=pdfs.background_defaults("dcs")
+            lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, domain, 0, 0, 0), *domain
         )[0],
         1.0,
     )
 
+    assert np.isclose(
+        quad(
+            lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, domain, 3, 0, 0), *domain
+        )[0],
+        1.0,
+    )
 
-def test_reduced_signal_normalised():
-    """
-    Check the reduced signal pdf is normalised
+    assert np.isclose(
+        quad(
+            lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, domain, 3, 2, 0), *domain
+        )[0],
+        1.0,
+    )
 
-    """
-    params = (146, 0.2, 0.2, 0.2, 0.2, 0.002)
-    points = definitions.reduced_mass_bins(1000)
-
-    integral = stats.integral(points, pdfs.normalised_signal_reduced(points, *params))
-
-    assert np.isclose(integral, 1.0)
-
-
-def test_reduced_model_normalised():
-    """
-    Check that the reduced pdf is normalised correctly
-
-    """
-    n_sig = 1000
-    n_bkg = 10000
-
-    params = (146, 0.2, 0.2, 0.2, 0.2, 0.002, 0.004, -0.001)
-    points = definitions.reduced_mass_bins(1000)
-
-    integral = stats.integral(points, pdfs.model_reduced(points, n_sig, n_bkg, *params))
-
-    assert np.isclose(integral, n_sig + n_bkg)
+    assert np.isclose(
+        quad(
+            lambda x: pdfs.estimated_bkg(x, estimated_bkg_pdf, domain, 3, 2, 1), *domain
+        )[0],
+        1.0,
+    )
