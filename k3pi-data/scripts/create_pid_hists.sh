cc4b2fc78844540c9cffee0a73beb83e0f2fca97
Richard Lane <richard.lane@bristol.ac.uk>
2023-04-04 18:25:54 +0200

    use the right cuts in pidcalib

--- k3pi-data/scripts/create_pid_hists.sh
+++ k3pi-data/scripts/create_pid_hists.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Create pidcalib2 hists, using what I think are the correct cuts
+# This wiki page is probably relevant
+# https://twiki.cern.ch/twiki/bin/view/LHCb/PIDCalibPackage
 
 # 2018 magdown
-lb-conda pidcalib pidcalib2.make_eff_hists --sample Turbo18 --magnet down --particle Pi --pid-cut "probe_PIDK < 0" --bin-var P --bin-var ETA --max-files 5
-lb-conda pidcalib pidcalib2.make_eff_hists --sample Turbo18 --magnet down --particle K --pid-cut "probe_PIDK > 8" --bin-var P --bin-var ETA  --max-files 5
+lb-conda pidcalib pidcalib2.make_eff_hists --sample Turbo18 --magnet down --particle Pi --pid-cut "DLLK < 0.0" --bin-var P --bin-var ETA --max-files 5
+lb-conda pidcalib pidcalib2.make_eff_hists --sample Turbo18 --magnet down --particle K --pid-cut "DLLK > 8.0" --bin-var P --bin-var ETA  --max-files 5
