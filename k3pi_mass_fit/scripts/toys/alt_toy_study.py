d06b42b6b65cb88f910819081b6fc20a4d3380ad
Richard Lane <richard.lane@bristol.ac.uk>
2023-04-04 17:11:43 +0100

    make the alt bkg pull study work

--- k3pi_mass_fit/scripts/toys/alt_toy_study.py
+++ k3pi_mass_fit/scripts/toys/alt_toy_study.py
@@ -8,7 +8,7 @@ import sys
 import time
 import pathlib
 import argparse
-from typing import Tuple
+from typing import Tuple, Callable
 from multiprocessing import Process, Manager
 
 from tqdm import tqdm
@@ -40,7 +40,7 @@ def _bins(n_underflow: int = 3) -> np.ndarray:
     gen_low, gen_high = pdfs.domain()
 
     return definitions.nonuniform_mass_bins(
-        (gen_low, fit_low, 145.0, 147.0, gen_high), (n_underflow, 30, 50, 30)
+        (gen_low, fit_low, gen_high), (n_underflow, 150)
     )
 
 
@@ -50,7 +50,8 @@ def _plot_fit(
     ws_combined,
     rs_fit_axes,
     ws_fit_axes,
-    fit_range,
+    rs_bkg_pdf,
+    ws_bkg_pdf,
 ):
     """
     Plot fit on an axis
@@ -58,22 +59,19 @@ def _plot_fit(
     """
     bins = _bins()
 
-    for axes, data, params, sign in zip(
+    for axes, data, params, pdf in zip(
         (rs_fit_axes, ws_fit_axes),
         (rs_combined, ws_combined),
         util.alt_rs_ws_params(fit_params),
-        ("cf", "dcs"),
+        (rs_bkg_pdf, ws_bkg_pdf),
     ):
         plotting.alt_bkg_fit(
             axes,
             *stats.counts(data, bins),
             bins,
-            fit_range,
+            pdf,
+            pdfs.reduced_domain(),
             params,
-            year="2018",
-            magnetisation="magdown",
-            sign=sign,
-            bdt_cut=False,
         )
 
 
@@ -101,11 +99,8 @@ def _gen(
     rng: np.random.Generator,
     n_sig: int,
     n_bkg: int,
-    year: str,
-    magnetisation: str,
+    bkg_pdf: Callable,
     sign: str,
-    bdt_cut: str,
-    bins: np.ndarray,
 ):
     """
     Generate points
@@ -113,12 +108,11 @@ def _gen(
     Same signal model, slightly different bkg models
 
     """
-    sig = toy_utils.gen_sig(rng, n_sig, pdfs.reduced_domain(), _sig_params())
+    sig = toy_utils.gen_sig(rng, n_sig, _sig_params(), pdfs.reduced_domain())
 
     # Get bkg pdf across the whole generator region
-    bkg_pdf = bkg.pdf(bins, year, magnetisation, sign, bdt_cut=bdt_cut)
     bkg_pts = toy_utils.gen_alt_bkg(
-        rng, n_bkg, bkg_pdf, _bkg_params(sign), (bins[0], bins[-1])
+        rng, n_bkg, bkg_pdf, _bkg_params(sign), pdfs.reduced_domain()
     )
 
     return np.concatenate((sig, bkg_pts))
@@ -126,7 +120,10 @@ def _gen(
 
 def _pull(
     rng: np.random.Generator,
-    fit_range: Tuple[float, float],
+    bins: np.ndarray,
+    n_underflow: int,
+    rs_bkg_pdf: Callable,
+    ws_bkg_pdf: Callable,
     out_dict: dict,
 ) -> np.ndarray:
     """
@@ -135,33 +132,19 @@ def _pull(
     Returns array of pulls
 
     """
-    rs_n_sig, ws_n_sig, n_bkg = 15_600_000, 55000, 50000
+    rs_n_sig, ws_n_sig, n_bkg = 800_000, 2_200, 25_000
 
     # Generate stuff
-    n_underflow = 3
-    bins = _bins(n_underflow)
-    gen_params = {
-        "year": "2018",
-        "magnetisation": "magdown",
-        "bdt_cut": False,
-        "bins": bins,
-    }
-    rs_combined = _gen(rng, rs_n_sig, n_bkg, sign="cf", **gen_params)
-    ws_combined = _gen(rng, ws_n_sig, n_bkg, sign="dcs", **gen_params)
+    rs_combined = _gen(rng, rs_n_sig, n_bkg, rs_bkg_pdf, "cf")
+    ws_combined = _gen(rng, ws_n_sig, n_bkg, ws_bkg_pdf, "dcs")
 
     # Perform fit
-    # TODO change these
-    rs_sig_expected = rs_n_sig
-    rs_bkg_expected = n_bkg
-    ws_sig_expected = ws_n_sig
-    ws_bkg_expected = n_bkg
-
     true_params = np.array(
         (
-            rs_sig_expected,
-            rs_bkg_expected,
-            ws_sig_expected,
-            ws_bkg_expected,
+            rs_n_sig,
+            n_bkg,
+            ws_n_sig,
+            n_bkg,
             *_sig_params(),
             *_bkg_params("cf"),
             *_bkg_params("dcs"),
@@ -174,10 +157,9 @@ def _pull(
         rs_counts[n_underflow:],
         ws_counts[n_underflow:],
         bins[n_underflow:],
-        "2018",
-        "magdown",
         true_params,
-        bdt_cut=False,
+        rs_bkg_pdf,
+        ws_bkg_pdf,
     )
     if not fitter.valid:
         raise InvalidFitError
@@ -202,8 +184,11 @@ def _pull(
 
 
 def _pull_study(
+    bins: np.ndarray,
+    n_underflow: int,
     n_experiments: int,
-    fit_range: Tuple[float, float],
+    rs_bkg_pdf: Callable,
+    ws_bkg_pdf: Callable,
     out_list: list,
     out_dict: dict,
 ) -> None:
@@ -222,7 +207,10 @@ def _pull_study(
             try:
                 pulls = _pull(
                     rng,
-                    fit_range,
+                    bins,
+                    n_underflow,
+                    rs_bkg_pdf,
+                    ws_bkg_pdf,
                     out_dict,
                 )
                 break
@@ -313,11 +301,15 @@ def _do_pull_study():
     Do the pull study
 
     """
-    fit_range = pdfs.reduced_domain()
-
     manager = Manager()
     out_list = manager.list()
     out_dict = manager.dict()
+
+    n_underflow = 3
+    bins = _bins(n_underflow)
+
+    rs_bkg_pdf = bkg.pdf(bins[n_underflow:], "2018", "magdown", "cf", bdt_cut=True)
+    ws_bkg_pdf = bkg.pdf(bins[n_underflow:], "2018", "magdown", "dcs", bdt_cut=True)
 
     # For plotting pulls
     fig, axes = plt.subplot_mosaic(
@@ -330,8 +322,11 @@ def _do_pull_study():
         Process(
             target=_pull_study,
             args=(
+                bins,
+                n_underflow,
                 n_experiments,
-                fit_range,
+                rs_bkg_pdf,
+                ws_bkg_pdf,
                 out_list,
                 out_dict,
             ),
@@ -364,14 +359,15 @@ def _do_pull_study():
         out_dict["ws_combined"],
         (axes["C"], axes["E"]),
         (axes["D"], axes["F"]),
-        fit_range,
+        rs_bkg_pdf,
+        ws_bkg_pdf,
     )
     for axis in (axes["C"], axes["D"]):
         axis.set_title("Example fit")
     fig.tight_layout()
 
     n_bins = len(_bins()) - 1
-    path = f"simul_pulls_{n_procs=}_{n_experiments=}_{n_bins=}.png"
+    path = f"alt_simul_pulls_{n_procs=}_{n_experiments=}_{n_bins=}.png"
     print(f"plotting {path}")
     fig.savefig(path)
 
