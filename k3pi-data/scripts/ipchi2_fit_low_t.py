1c17ce803e84679ec98e635c69d7e0970134010b
Richard Lane <richard.lane@bristol.ac.uk>
2023-04-11 10:48:52 +0100

    change the params in the ipchi2 fit to work

--- k3pi-data/scripts/ipchi2_fit_low_t.py
+++ k3pi-data/scripts/ipchi2_fit_low_t.py
@@ -30,6 +30,15 @@ def _fit(log_ipchi2s: np.ndarray, param_guess: dict) -> Minuit:
 
     fitter = Minuit(cost_fcn, **param_guess)
 
+    fitter.limits = (
+        (0.8, 2.5),  # Centre
+        (0.8, 2.5),  # width L
+        (0.8, 2.5),  # width R
+        (0.0, 2.0),  # alpha L
+        (0.0, 2.0),  # alpha R
+        (None, None),  # Beta (fixed below)
+    )
+
     fitter.fixed["beta"] = True
     fitter.migrad(ncall=5000)
 
@@ -69,15 +78,19 @@ def main(*, sign: str):
     ipchi2s = ipchi2s[ipchi2s > low_ip]
     ipchi2s = ipchi2s[ipchi2s < high_ip]
 
+    # Only use first N pts
+    n_pts = 1_000_000
+    ipchi2s = ipchi2s[:n_pts]
+
     # fit
     fitter = _fit(
         ipchi2s,
         {
-            "centre": 1.5,
-            "width_l": 2.5,
-            "width_r": 2.5,
-            "alpha_l": 0.1,
-            "alpha_r": 0.1,
+            "centre": 1.45,
+            "width_l": 1.0,
+            "width_r": 1.0,
+            "alpha_l": 0.0,
+            "alpha_r": 0.0,
             "beta": 0.0,
         },
     )
