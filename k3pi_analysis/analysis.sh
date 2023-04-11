749bb3ce3870512dc7b78ff191d6e35b32251cca
Richard Lane <richard.lane@bristol.ac.uk>
2023-04-11 18:31:34 +0200

    capitalise mag

--- k3pi_analysis/analysis.sh
+++ k3pi_analysis/analysis.sh
@@ -3,6 +3,8 @@
 YEAR=2018
 MAG=magdown
 
+echo $YEAR $MAG
+
 # Run the whole analysis
 # Gets the data, trains "BDT-cut" classifier, trains efficiency reweighter,
 # performs mass fits and a fit to the yield
@@ -22,15 +24,15 @@ python k3pi-data/create_ampgen.py ../rs_Dbar02piKpipi.root cf &
 pids[1]=$!
 
 # Only create a few pgun dfs for speed
-python k3pi-data/create_pgun.py $YEAR dcs $mag -v &
+python k3pi-data/create_pgun.py $YEAR dcs $MAG -v &
 pids[2]=$!
-python k3pi-data/create_pgun.py $YEAR cf $mag -v &
+python k3pi-data/create_pgun.py $YEAR cf $MAG -v &
 pids[3]=$!
 
 # MC dataframes are quick so just stick them in here
-python k3pi-data/create_mc.py $YEAR dcs $mag &
+python k3pi-data/create_mc.py $YEAR dcs $MAG &
 pids[4]=$!
-python k3pi-data/create_mc.py $YEAR cf $mag &
+python k3pi-data/create_mc.py $YEAR cf $MAG &
 pids[5]=$!
 
 # Wait for ampgen and particle dfs to be made
@@ -40,15 +42,15 @@ done
 unset pids
 
 # Start training efficiency reweighters now because they are extremely slow
-python k3pi_efficiency/create_reweighter.py --cut cf $YEAR $mag both &
+python k3pi_efficiency/create_reweighter.py --cut cf $YEAR $MAG both &
 pids[0]=$!
-python k3pi_efficiency/create_reweighter.py --cut dcs $YEAR $mag both &
+python k3pi_efficiency/create_reweighter.py --cut dcs $YEAR $MAG both &
 pids[1]=$!
 
 # Only create a few real dfs for speed
-python k3pi-data/create_real.py -n 48 $YEAR cf $mag --n_procs 3 &
+python k3pi-data/create_real.py -n 48 $YEAR cf $MAG --n_procs 3 &
 pids[2]=$!
-python k3pi-data/create_real.py -n 48 $YEAR dcs $mag --n_procs 3 &
+python k3pi-data/create_real.py -n 48 $YEAR dcs $MAG --n_procs 3 &
 pids[3]=$!
 
 # This should be most of the analysis, in terms of time
@@ -61,15 +63,15 @@ unset pids
 ./k3pi_efficiency/lib_efficiency/amplitude_models/build.sh
 
 # Create some uppermass dfs
-python k3pi-data/create_uppermass.py $YEAR dcs $mag -n 48 &
+python k3pi-data/create_uppermass.py $YEAR dcs $MAG -n 48 &
 pids[0]=$!
-python k3pi-data/create_uppermass.py $YEAR cf $mag -n 48 &
+python k3pi-data/create_uppermass.py $YEAR cf $MAG -n 48 &
 pids[1]=$!
 
 # Add phsp information to the real data dfs
-python k3pi-data/scripts/add_phsp_bins.py $YEAR dcs $mag &
+python k3pi-data/scripts/add_phsp_bins.py $YEAR dcs $MAG &
 pids[2]=$!
-python k3pi-data/scripts/add_phsp_bins.py $YEAR cf $mag &
+python k3pi-data/scripts/add_phsp_bins.py $YEAR cf $MAG &
 pids[3]=$!
 for pid in ${pids[*]}; do
     wait $pid
@@ -80,19 +82,19 @@ unset pids
 python k3pi-data/scripts/plot_parameterisation.py
 
 # Train BDT
-python k3pi_signal_cuts/create_classifier.py $YEAR dcs $mag &
+python k3pi_signal_cuts/create_classifier.py $YEAR dcs $MAG &
 pids[0]=$!
 
 # Perform mass fits without BDT cut
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 0 &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 0 &
 pids[1]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 1 &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 1 &
 pids[2]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 2 &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 2 &
 pids[3]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 3 &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 3 &
 pids[4]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG &
 pids[5]=$!
 for pid in ${pids[*]}; do
     wait $pid
@@ -106,15 +108,15 @@ python k3pi_signal_cuts/scripts/plot_cuts.py
 python k3pi_signal_cuts/scripts/plot_data_cuts.py
 
 # Perform mass fits with BDT cut
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 0 --bdt_cut &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 0 --bdt_cut &
 pids[0]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 1 --bdt_cut &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 1 --bdt_cut &
 pids[1]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 2 --bdt_cut &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 2 --bdt_cut &
 pids[2]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 3 --bdt_cut &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 3 --bdt_cut &
 pids[3]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag --bdt_cut &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG --bdt_cut &
 pids[4]=$!
 for pid in ${pids[*]}; do
     wait $pid
@@ -122,15 +124,15 @@ done
 unset pids
 
 # Perform mass fits with BDT + efficiency
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 0 --bdt_cut --efficiency &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 0 --bdt_cut --efficiency &
 pids[0]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 1 --bdt_cut --efficiency &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 1 --bdt_cut --efficiency &
 pids[1]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 2 --bdt_cut --efficiency &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 2 --bdt_cut --efficiency &
 pids[2]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag 3 --bdt_cut --efficiency &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG 3 --bdt_cut --efficiency &
 pids[3]=$!
-python k3pi_mass_fit/scripts/data_fits.py $YEAR $mag --bdt_cut --efficiency &
+python k3pi_mass_fit/scripts/data_fits.py $YEAR $MAG --bdt_cut --efficiency &
 pids[4]=$!
 for pid in ${pids[*]}; do
     wait $pid
@@ -138,23 +140,23 @@ done
 unset pids
 
 # Plot yields
-python k3pi_mass_fit/scripts/plot_yield_from_file.py $YEAR $mag 0 1 2 3 --integrated
-python k3pi_mass_fit/scripts/plot_yield_from_file.py $YEAR $mag 0 1 2 3 --integrated --bdt_cut
-python k3pi_mass_fit/scripts/plot_yield_from_file.py $YEAR $mag 0 1 2 3 --integrated --bdt_cut --efficiency
+python k3pi_mass_fit/scripts/plot_yield_from_file.py $YEAR $MAG 0 1 2 3 --integrated
+python k3pi_mass_fit/scripts/plot_yield_from_file.py $YEAR $MAG 0 1 2 3 --integrated --bdt_cut
+python k3pi_mass_fit/scripts/plot_yield_from_file.py $YEAR $MAG 0 1 2 3 --integrated --bdt_cut --efficiency
 
 # Plot fits
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 0 --bdt_cut
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 1 --bdt_cut
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 2 --bdt_cut
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 3 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 0 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 1 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 2 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 3 --bdt_cut
 
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 0 --bdt_cut
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 1 --bdt_cut
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 2 --bdt_cut
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 3 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 0 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 1 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 2 --bdt_cut
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 3 --bdt_cut
 
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 0 --bdt_cut --efficiency
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 1 --bdt_cut --efficiency
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 2 --bdt_cut --efficiency
-python k3pi_fitter/scripts/fit_from_file.py $YEAR $mag 3 --bdt_cut --efficiency
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 0 --bdt_cut --efficiency
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 1 --bdt_cut --efficiency
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 2 --bdt_cut --efficiency
+python k3pi_fitter/scripts/fit_from_file.py $YEAR $MAG 3 --bdt_cut --efficiency
 
