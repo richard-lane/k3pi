0359918bba46aaf19bff29382e77857093cdc204
Richard Lane <richard.lane@bristol.ac.uk>
2023-04-25 18:07:11 +0100

    plot data thresholds with the new bdt cut val

--- k3pi_signal_cuts/scripts/plot_data_threshholds.py
+++ k3pi_signal_cuts/scripts/plot_data_threshholds.py
@@ -4,21 +4,19 @@ Plot delta M in data at various signal significances
 """
 import sys
 import pathlib
+from itertools import islice
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 sys.path.append(str(pathlib.Path(__file__).absolute().parents[1]))
 sys.path.append(str(pathlib.Path(__file__).absolute().parents[2] / "k3pi-data"))
+sys.path.append(str(pathlib.Path(__file__).absolute().parents[2] / "k3pi_mass_fit"))
 
 from lib_cuts.get import classifier as get_clf
 from lib_cuts import definitions
 from lib_data import get, training_vars
-
-
-def _delta_m(data: pd.DataFrame) -> np.ndarray:
-    """D* - D0 Mass"""
-    return data["D* mass"] - data["D0 mass"]
+from libFit.util import delta_m as dm_fcn
 
 
 def _plot(axis: plt.Axes, delta_m: np.ndarray, threshhold: float) -> None:
@@ -34,14 +32,13 @@ def _plot(axis: plt.Axes, delta_m: np.ndarray, threshhold: float) -> None:
 
 def main():
     """
-    Plot DCS delta M for various values of the cut threshhold
-
-    For each, work out the signal significance
+    Plot delta M for various values of the cut threshhold
 
     """
+    n_dfs = 100
     year, magnetisation = "2018", "magdown"
-    ws_df = pd.concat(get.data(year, "dcs", magnetisation))
-    rs_df = pd.concat(get.data(year, "cf", magnetisation))
+    ws_df = pd.concat(islice(get.data(year, "dcs", magnetisation), n_dfs))
+    rs_df = pd.concat(islice(get.data(year, "cf", magnetisation), n_dfs))
 
     # Use the DCS classifier
     clf = get_clf(year, "dcs", magnetisation)
@@ -51,11 +48,14 @@ def main():
 
     # For various values of the threshhold, perform cuts
     # and plot the resultant delta M distribution
-    threshholds = [0.0, 0.05, definitions.THRESHOLD, 0.30, 0.50, 0.80, 1.0]
+    threshholds = [0.0, 0.05, definitions.THRESHOLD, 0.40, 0.50, 0.80, 1.0]
 
-    ws_delta_m = _delta_m(ws_df)
-    rs_delta_m = _delta_m(rs_df)
+    ws_delta_m = dm_fcn(ws_df)
+    rs_delta_m = dm_fcn(rs_df)
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Decided now to swap WS and RS plots
+    axes = axes[1], axes[0]
 
     for threshhold in threshholds:
         _plot(axes[0], ws_delta_m[ws_sig_probs > threshhold], threshhold)
@@ -72,7 +72,7 @@ def main():
 
     axes[0].text(
         146.2,
-        4000,
+        3500,
         "optimal significance\n(in simulation)",
         color="green",
         fontsize=8,
