1dd759f2df5153251e41b92127669d63f9b43a27
Richard Lane <richard.lane@bristol.ac.uk>
2023-04-25 19:35:38 +0200

    remember to transfer the bdt in script

--- k3pi_signal_cuts/scripts/data_significance/run.sh
+++ k3pi_signal_cuts/scripts/data_significance/run.sh
@@ -13,13 +13,17 @@ source python/bin/activate
 git clone https://github.com/richard-lane/k3pi.git
 cd k3pi/
 
+# Move the BDT to the right dir
+mkdir k3pi_signal_cuts/classifiers/
+mv ../2018_dcs_magdown.pkl k3pi_signal_cuts/classifiers/
+
 # Build the shared libraries that we'll need later for phsp bins
 ./k3pi_efficiency/lib_efficiency/amplitude_models/build.sh
 
 # Create real data dataframes now as well; this is also slow
-python k3pi-data/create_real.py 2018 dcs magdown --n_procs 4 -n 96
+python k3pi-data/create_real.py 2018 dcs magdown --n_procs 4
 pid1=$!
-python k3pi-data/create_real.py 2018 cf magdown --n_procs 4 -n 96
+python k3pi-data/create_real.py 2018 cf magdown --n_procs 4
 pid2=$!
 
 wait pid1
