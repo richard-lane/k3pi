8ce0f16e0f9e661269b1223675f71f2201a6eab5
Richard Lane <richard.lane@bristol.ac.uk>
2023-05-23 11:08:45 +0200

    fix pickle path

--- k3pi_fitter/scripts/fit_from_file.py
+++ k3pi_fitter/scripts/fit_from_file.py
@@ -294,8 +294,8 @@ def main(
     if not quiet:
         print(f"plotting {path}")
     fig.savefig(path)
-    with open(path, "wb") as f:
-        pickle.dump((fig, axes), f"plot_pkls/{path}.pkl")
+    with open(f"plot_pkls/{path}.pkl", "wb") as f:
+        pickle.dump((fig, axes), f)
 
 
 if __name__ == "__main__":
