2fd10f6f9204e40a2b6f3ac4c8ca51ec90e51734
Richard Lane <richard.lane@bristol.ac.uk>
2023-06-30 17:22:41 +0200

    change angle plot to be nicer

--- k3pi-data/scripts/plot_data_angle.py
+++ k3pi-data/scripts/plot_data_angle.py
@@ -10,20 +10,37 @@ import argparse
 
 import uproot
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
-from lib_data import definitions, util, cuts
+from lib_data import definitions, util, cuts, read
 
 
-def _angles(tree, prefix_1: str, prefix_2: str, keep: np.ndarray) -> np.ndarray:
+def _angles(dataframe: pd.DataFrame, prefix_1: str, prefix_2: str) -> np.ndarray:
     """
     Angles between hadrons
 
     """
-    return util.relative_angle_branches(tree, prefix_1, prefix_2)[keep]
+    suffices = "PX", "PY", "PZ"
+    p_1 = np.row_stack([dataframe[f"{prefix_1}_{suffix}"] for suffix in suffices])
+    p_2 = np.row_stack([dataframe[f"{prefix_2}_{suffix}"] for suffix in suffices])
+
+    return util.relative_angle(p_1, p_2)
+
+
+def _data_keep_no_angle(dataframe: pd.DataFrame) -> np.ndarray:
+    """Data cuts but not on the angle"""
+    return (
+        cuts._d0_mass_keep(dataframe)
+        & cuts._delta_m_keep(dataframe)
+        & cuts._l0_keep(dataframe)
+        & cuts._hlt_keep(dataframe)
+        & cuts._pid_keep(dataframe)
+        & cuts._ghost_keep(dataframe)
+    )
 
 
 def _plot(axis: plt.Axes, angles: np.ndarray, label: str) -> None:
@@ -50,10 +67,10 @@ def main(*, year: str, sign: str, magnetisation: str) -> None:
 
     """
     # Just use the first few files
-    n_files = 5
+    n_files = 10
     data_paths = definitions.data_files(year, magnetisation)[:n_files]
 
-    fig, axes = plt.subplots(2, 2, figsize=(12, 12), sharex=True, sharey=True)
+    fig, axes = plt.subplots(2, 2, figsize=(12, 8), sharex=True, sharey=True)
 
     angles = [[], [], [], []]
     with tqdm(total=n_files * 4) as pbar:
@@ -61,32 +78,31 @@ def main(*, year: str, sign: str, magnetisation: str) -> None:
             with uproot.open(path) as data_f:
                 # Find which events to keep (after cuts)
                 tree = data_f[definitions.data_tree(sign)]
-                keep = (
-                    cuts._trigger_keep(tree)
-                    & cuts._pid_keep(tree)
-                    & cuts._cands_keep(tree)
-                    & cuts._sanity_keep(tree)
-                )
+                dataframe = tree.arrays(read.branches("data"), library="pd")
+                dataframe = read.remove_refit(dataframe)
+
+                keep = _data_keep_no_angle(dataframe)
+                dataframe = dataframe[keep]
+                dataframe = cuts.cands_cut(dataframe)
 
                 for prefix, lst in zip(definitions.DATA_BRANCH_PREFICES[:-1], angles):
                     # Get the angle with the slow pi, plot it in the right colour on the right axis
                     lst.append(
                         _angles(
-                            tree,
+                            dataframe,
                             prefix,
                             definitions.DATA_BRANCH_PREFICES[-1],
-                            keep,
                         )
                     )
                     pbar.update(1)
 
-    for axis, angle, title in zip(
-        axes.ravel(), angles, definitions.DATA_BRANCH_PREFICES[:-1]
-    ):
+    titles = r"$K^\pm$", r"$\pi^\mp$", r"$\pi^\mp$", r"$\pi^\pm$"
+    for axis, angle, title in zip(axes.ravel(), angles, titles):
         _plot(axis, np.concatenate(angle), definitions.DATA_BRANCH_PREFICES[-1])
         axis.axvline(cuts.ANGLE_CUT_DEG, color="r", alpha=0.8)
         axis.set_title(title)
-        axis.legend()
+        axis.set_xlabel("$\pi_s$-Hadron Angle / $^\circ$")
+        axis.set_ylabel("Count")
 
     fig.tight_layout()
     path = f"data_angle_{year}_{sign}_{magnetisation}.png"
