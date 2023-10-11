0c694150f4b1e0a416324e75d28b5540d0d96f9c
Richard Lane <richard.lane@bristol.ac.uk>
2023-10-11 15:32:04 +0100

    plot both sig + bkg clf probs

--- k3pi_signal_cuts/scripts/plot_probabilities.py
+++ k3pi_signal_cuts/scripts/plot_probabilities.py
@@ -25,24 +25,41 @@ def main():
     sig_df = get.mc(year, sign, magnetisation)
     bkg_df = pd.concat(get.uppermass(year, sign, magnetisation))
 
-    # We only want the testing data here
-    sig_df = sig_df[~sig_df["train"]]
-    bkg_df = bkg_df[~bkg_df["train"]]
+    sig_train_df = sig_df[sig_df["train"]]
+    bkg_train_df = bkg_df[bkg_df["train"]]
 
     # Predict which of these are signal and background using our classifier
     clf = get_clf(year, sign, magnetisation)
     var_names = list(training_vars.training_var_names())
-    sig_proba = clf.predict_proba(sig_df[var_names])[:, 1]
-    bkg_proba = clf.predict_proba(bkg_df[var_names])[:, 1]
+
+    sig_train_proba = clf.predict_proba(sig_train_df[var_names])[:, 1]
+    bkg_train_proba = clf.predict_proba(bkg_train_df[var_names])[:, 1]
 
     # Plot histograms of our variables before/after doing these cuts
     fig, ax = plt.subplots()
     bins = np.linspace(0, 1, 100)
-    ax.hist(sig_proba, bins=bins, label="sig")
-    ax.hist(bkg_proba, bins=bins, label="bkg")
+    hist_kw = {"histtype": "step", "bins": bins, "density": True}
+
+    ax.hist(sig_train_proba, **hist_kw, label="Signal (Train)", color="k")
+    ax.hist(bkg_train_proba, **hist_kw, label="Background (Train)", color="r")
+
+    # Repeat with testing data
+    sig_test_df = sig_df[~sig_df["train"]]
+    bkg_test_df = bkg_df[~bkg_df["train"]]
+
+    sig_test_proba = clf.predict_proba(sig_test_df[var_names])[:, 1]
+    bkg_test_proba = clf.predict_proba(bkg_test_df[var_names])[:, 1]
+
+    hist_kw["linestyle"] = "--"
+    ax.hist(sig_test_proba, **hist_kw, label="Signal (Test)", color="k")
+    ax.hist(bkg_test_proba, **hist_kw, label="Background (Test)", color="r")
 
     ax.legend()
-    fig.suptitle("signal probability")
+    ax.set_yticks([])
+    ax.set_ylabel("Counts")
+
+    fig.tight_layout()
+    fig.savefig("clf_probabilities.png")
 
     plt.show()
 
